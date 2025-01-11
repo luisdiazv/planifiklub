a8e6494b766dd5166ed84973c106a1470cd8bcc6
Jonathan Ochoa <jsochoac@unal.edu.co>
2025-01-10 22:35:04 -0500

    Balanza de pagos
    Balanza de pagos funcional en forma de modal. Faltantes, que el boton no genere otro boton y la redirección adecuada al terminar el pago, funcionara cuando el site no este como local.

--- src/App.js
+++ src/App.js
@@ -10,11 +10,20 @@ import AboutPk from './routes/AboutPk.jsx';  // Asegúrate de que la importació
 import LogIn from './routes/LogIn.jsx';  // Asegúrate de que la importación sea correcta
 import SignUp from './routes/SignUp.jsx';  // Asegúrate de que la importación sea correcta
 import NotFound from './routes/NotFound.jsx';  // Asegúrate de que la importación sea correcta
-
+//import Balanzadepagos from './Views/Balanzadepagos.js'
 function App() {
+  //Prueba balanza de pagos con mercadopago. Para realizar la prueba es necesario comentar la segunda
+  //funcion return y descomentar la primera
+  /*
+  return(
+    <>
+    <Balanzadepagos />
+    </>
+  )
+} */
 
   return (
-    <>
+        <>
 
       <div className="App">
         <Navbar />
@@ -24,7 +33,7 @@ function App() {
             <Route path="/AboutUs" element={<AboutUs />} />
             <Route path="/AboutPk" element={<AboutPk />} />
             <Route path="/LogIn" element={<LogIn />} />
-            <Route path="/SignUp" element={<SignUp />} />
+            <Route path="/SignUp" element={<SignUp />} />*/
             <Route path="*" element={<NotFound />} />  {/* Ruta no encontrada */}
           </Routes>
         </div>
@@ -32,8 +41,10 @@ function App() {
       </div>
 
     </>
+    
 
   );
+  
 }
 
 export default App;
