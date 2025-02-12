896ec6d97d48fa7ea9424d84c1e5704d402c41d2
StoryChara <mjarah@unal.edu.co>
2025-02-12 10:53:27 -0500

    Generar factura

--- src/routes/ShowEvent.jsx
+++ src/routes/ShowEvent.jsx
@@ -4,6 +4,7 @@ import { getEventType } from "../Ctrl/TiposEventosCtrl";
 import { getNombresApellidosById } from "../Ctrl/UsuarioCtrl";
 import {getPedidosByIdEvento} from "../Ctrl/PedidoCtrl";
 import { getEdificiosByIdEvento } from "../Ctrl/EdificiosCtrl";
+import { getPagosbyEventID } from "../Ctrl/PagosCtrl";
 import "./ShowEventStyles.css";
 
 import { getEventById, updateEventStatus } from "../Ctrl/EventosCtrl";
@@ -13,8 +14,6 @@ import * as pdfMake from 'pdfmake/build/pdfmake';
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
 pdfMake.vfs = pdfFonts;
-
-const event_ID = 2;
 
 //pdfMake.vfs = { ...pdfFonts, ...customFonts };
 
@@ -117,7 +116,6 @@ const ShowEvent = () => {
     const generatePDF = async () => {
         try {
             setLoading(true);
-            console.log(pdfMake.vfs);
     
             // Construye el contenido HTML dinámico basado en la información del evento
             const htmlContent = `
@@ -180,18 +178,53 @@ const ShowEvent = () => {
             const documentDefinition = {
                 content: pdfContent,
                 styles: {
-                    header: { fontSize: 18, margin: [0, 10, 0, 10], font: "ScheherazadeNew-Regular" },
-                    subheader: { fontSize: 14, bold: true, margin: [0, 10, 0, 5], font: "ScheherazadeNew-Regular" },
-                    paragraph: { fontSize: 12, margin: [0, 5, 0, 5], font: "ScheherazadeNew-Regular" },
+                    header: { fontSize: 18, margin: [0, 10, 0, 10]},
+                    subheader: { fontSize: 14, bold: true, margin: [0, 10, 0, 5]},
+                    paragraph: { fontSize: 12, margin: [0, 5, 0, 5]},
                 },
             };
     
             // Genera y descarga el PDF
-            pdfMake.createPdf(documentDefinition).download(`Reporte_Evento_${event_ID}.pdf`);
+            pdfMake.createPdf(documentDefinition).download(`Reporte_Evento_${id}.pdf`);
     
         } catch (error) {
             console.error("Error generando el reporte en PDF:", error.message);
             alert("Ocurrió un error al generar el reporte. Inténtalo nuevamente.");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const generateBill = async () => {
+        try {
+            setLoading(true);
+            const info = getPagosbyEventID(id);
+            console.log(info);
+    
+            // Construye el contenido HTML dinámico basado en la información del evento
+            const htmlContent = `
+                test
+            `;
+    
+            // Convierte el contenido HTML a formato PDFMake
+            const pdfContent = htmlToPdfMake(htmlContent);
+    
+            // Configuración básica del documento PDF
+            const documentDefinition = {
+                content: pdfContent,
+                styles: {
+                    header: { fontSize: 18, margin: [0, 10, 0, 10]},
+                    subheader: { fontSize: 14, bold: true, margin: [0, 10, 0, 5]},
+                    paragraph: { fontSize: 12, margin: [0, 5, 0, 5]},
+                },
+            };
+    
+            // Genera y descarga el PDF
+            pdfMake.createPdf(documentDefinition).download(`Facturacion_Evento_${id}.pdf`);
+    
+        } catch (error) {
+            console.error("Error generando la facturación en PDF:", error.message);
+            alert("Ocurrió un error al generar la facturacion. Inténtalo nuevamente.");
         } finally {
             setLoading(false);
         }
@@ -270,6 +303,17 @@ const ShowEvent = () => {
             }}
         >
             {loading ? "Generando PDF..." : "Generar Reporte en PDF"}
+        </button>
+
+        <button
+            onClick={generateBill}
+            disabled={loading} // Deshabilitar el botón mientras se genera el PDF
+            style={{
+                cursor: loading ? "not-allowed" : "pointer",
+                opacity: loading ? 0.6 : 1,
+            }}
+        >
+            {loading ? "Generando Facturación..." : "Generar Facturación en PDF"}
         </button>
 
         </div>
