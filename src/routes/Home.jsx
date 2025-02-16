5e8dd417d289ff1deb98d74251a425c35a24379a
sergio <saparadaa@unal.edu.co>
2025-02-16 12:15:08 -0500

    responsive font size

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,8 +6,7 @@ import image1 from '../Components/imgs/homeSliderImage1.jpg'
 import image2 from '../Components/imgs/homeSliderImage2.jpg'
 import image3 from '../Components/imgs/homeSliderImage3.jpg'
 import { Link } from "react-router-dom";
-//import contentImg2 from '../Components/imgs/homeSliderImage1.jpg'
-import contentImg1 from '../Components/imgs/why.jpeg'
+import contentImg1 from '../Components/imgs/homeImg1.jpg'
 
 const images = [
     {
