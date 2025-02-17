a60a2cccb7a6fcf5d6c665e78eacc0ca47a49632
Luis Diaz <ludiazv@unal.edu.co>
2025-02-17 10:41:42 -0500

    Configurador base de edificios realizado

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ import CalendarioEventos from './routes/CalendarioEventos.jsx';
 import ConfiguradorRoles from './routes/Service/RolesService.jsx';
 import ConfiguradorSocio from './routes/Service/MemberService.jsx';
 import ConfiguradorProductos from './routes/Service/ProductosService.jsx';
+import ConfiguradorEdificios from './routes/Service/EdificiosService.jsx';
+
 
 //Ruta no encontrada
 import NotFound from './routes/NotFound.jsx';
@@ -61,9 +63,10 @@ function App() {
             <Route path="/app/evento/:id" element={<ShowEvent />} />  {/* Ruta para evento específico */}
 
             {/* Configuradores */}
-            <Route path="/app/RolesService" element={<ConfiguradorRoles />} /> {/*Solo Admin */}
-            <Route path="/app/SociosService" element={<ConfiguradorSocio />} /> {/*Solo Admin */}
-            <Route path="/app/ProductosService" element={<ConfiguradorProductos />} /> {/*Solo Admin */}
+            <Route path="/app/rolesConfig" element={<ConfiguradorRoles />} /> {/*Solo Admin */}
+            <Route path="/app/sociosConfig" element={<ConfiguradorSocio />} /> {/*Solo Admin */}
+            <Route path="/app/productosConfig" element={<ConfiguradorProductos />} /> {/*Solo Admin */}
+            <Route path="/app/edificiosConfig" element={<ConfiguradorEdificios />} /> {/*Solo Admin */}
 
             {/* Ruta no encontrada */}
             <Route path="*" element={<NotFound />} />
