ab542bf450255c824e17d2960a96bd62ef2ab80e
sergio <saparadaa@unal.edu.co>
2025-02-22 16:04:42 -0500

    eventos y contizaciones, config de roles responsive

--- src/routes/Service/RolesService.jsx
+++ src/routes/Service/RolesService.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { getUsuarioByID } from '../../Ctrl/UsuarioCtrl';
 import { getAllRoles } from '../../Ctrl/RolCtrl'; // Asegúrate de que esta ruta sea correcta
 import { getRolByUser, updateAccesos } from '../../Ctrl/AccesosCtrl';
-
+import "./RolesServiceStyles.css";
 const ConfiguradorRoles = () => {
   const [userId, setUserId] = useState('');
   const [userInfo, setUserInfo] = useState(null);
@@ -70,149 +70,63 @@ const ConfiguradorRoles = () => {
   };
 
   return (
-    <div style={styles.container}>
-      <header style={styles.header}>Buscador de Usuario</header>
-      <div style={styles.inputContainer}>
-        <input
-          type="text"
-          placeholder="Ingrese el ID del usuario"
-          value={userId}
-          onChange={(e) => setUserId(e.target.value)}
-          style={styles.input}
-        />
-        <button onClick={handleSearchUser} style={styles.button}>
-          Buscar Usuario
-        </button>
-      </div>
-      {error && <p style={styles.error}>{error}</p>}
-      {userInfo && (
-        <div style={styles.userInfo}>
-          <p><strong>ID Usuario:</strong> {userInfo.idusuario}</p>
-          <p><strong>Nombre:</strong> {userInfo.nombres}</p>
-          <p><strong>{userInfo.tipo_documento === 'NIT' ? 'Encargado:' : 'Apellido:'}</strong> {userInfo.apellidos}</p>
-          <p><strong>Correo:</strong> {userInfo.correo}</p>
+    <div className="fullConfig-container">
+      <div className="configurador-roles-container">
+        <h2 className="configurador-roles-header">Buscador de Usuario</h2>
+        <div className="configurador-roles-input-container">
+          <input
+            type="text"
+            placeholder="Ingrese el ID del usuario"
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
+            className="configurador-roles-input"
+          />
+          <button onClick={handleSearchUser} className="configurador-roles-button">
+            Buscar Usuario
+          </button>
         </div>
-      )}
-      {/* Mostrar los checkboxes para los roles */}
-      {roles.length > 0 && userInfo && (
-        <div style={styles.rolesContainer}>
-          <h3>Asignar Roles al Usuario</h3>
-          {roles.map(role => (
-            <div key={role.id} style={styles.checkboxContainer}>
-              <input
-                type="checkbox"
-                id={`role-${role.idroles}`}
-                checked={selectedRoles.includes(role.idroles)} // Si el id del rol está en selectedRoles, lo selecciona
-                onChange={() => handleRoleChange(role.idroles)}
-                style={styles.checkbox}
-              />
-              <label htmlFor={`role-${role.idroles}`} style={styles.label}>{role.nombre_rol}</label>
+        {error && <p className="configurador-roles-error">{error}</p>}
+        {userInfo && (
+          <div className="configurador-roles-user-info">
+            <p><strong>ID Usuario:</strong> {userInfo.idusuario}</p>
+            <p><strong>Nombre:</strong> {userInfo.nombres}</p>
+            <p><strong>{userInfo.tipo_documento === 'NIT' ? 'Encargado:' : 'Apellido:'}</strong> {userInfo.apellidos}</p>
+            <p><strong>Correo:</strong> {userInfo.correo}</p>
+          </div>
+        )}
+        {roles.length > 0 && userInfo && (
+          <div className="configurador-roles-roles-container">
+            <div className="config-roles-roles-text">
+              <h3>Asignar Roles al Usuario</h3>
+              <div className="configurador-roles-checkbox-container">
+                {roles.map(role => (
+                  <div key={role.id} className="configurador-roles-checkbox-content">
+                    <div className="basic-input-checkbox-container"><input
+                      type="checkbox"
+                      id={`role-${role.idroles}`}
+                      checked={selectedRoles.includes(role.idroles)}
+                      onChange={() => handleRoleChange(role.idroles)}
+                      className="basic-input-checkbox"
+                    /></div>
+                    <label htmlFor={`role-${role.idroles}`} className="configurador-roles-label">{role.nombre_rol}</label>
+                  </div>
+                ))}
+              </div>
             </div>
-        ))}
-            {/* Botones de guardar y salir */}
-            <div style={styles.buttonContainer}>
-                <button onClick={handleSave} style={styles.saveButton}>
-                Guardar
-                </button>
-                <button onClick={handleExitWithoutSaving} style={styles.exitButton}>
-                Salir sin guardar
-                </button>
-            </div>
-        </div>
-      )}
 
+            <div className="configurador-roles-button-container">
+              <button onClick={handleSave} className="configurador-roles-save-button">
+                Guardar
+              </button>
+              <button onClick={handleExitWithoutSaving} className="configurador-roles-exit-button">
+                Salir sin guardar
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    fontFamily: 'Arial, sans-serif',
-    padding: '20px',
-    maxWidth: '400px',
-    margin: '0 auto',
-    backgroundColor: '#f9f9f9',
-    borderRadius: '10px',
-    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-  },
-  header: {
-    fontSize: '24px',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: '20px',
-  },
-  inputContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    marginBottom: '20px',
-  },
-  input: {
-    flex: 1,
-    marginRight: '10px',
-    padding: '10px',
-    borderRadius: '5px',
-    border: '1px solid #ccc',
-  },
-  button: {
-    padding: '10px 20px',
-    backgroundColor: '#800000',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
-  },
-  error: {
-    color: 'red',
-    textAlign: 'center',
-    marginBottom: '20px',
-  },
-  userInfo: {
-    padding: '10px',
-    backgroundColor: '#fff',
-    borderRadius: '5px',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-    fontSize: '14px',
-  },
-  rolesContainer: {
-    marginTop: '20px',
-    padding: '10px',
-    backgroundColor: '#fff',
-    borderRadius: '5px',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-    fontSize: '14px',
-  },
-  checkboxContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    marginBottom: '10px',
-  },
-  checkbox: {
-    marginRight: '10px',
-  },
-  label: {
-    fontSize: '14px',
-  },
-  buttonContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    marginTop: '20px',
-  },
-  saveButton: {
-    padding: '10px 20px',
-    backgroundColor: '#4CAF50',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
-  },
-  exitButton: {
-    padding: '10px 20px',
-    backgroundColor: '#f44336',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
-  },
 };
 
 export default ConfiguradorRoles;
