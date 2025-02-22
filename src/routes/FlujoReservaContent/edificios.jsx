00f4d960bfec5bd915869b280e38de7a78be4900
Luis Diaz <ludiazv@unal.edu.co>
2025-02-21 22:37:40 -0500

    Fix/Error en el alert de edificios

--- src/routes/FlujoReservaContent/edificios.jsx
+++ src/routes/FlujoReservaContent/edificios.jsx
@@ -68,7 +68,7 @@ const EdificiosList = () => {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    //e.preventDefault();
 
     if (selectedEdificios.size === 0) {
       window.alert("Debes seleccionar al menos un edificio para reservar en tu evento.");
@@ -87,17 +87,18 @@ const EdificiosList = () => {
 
       if (!idMontaje) {
         window.alert(`Debes seleccionar un montaje para el edificio: ${edificio?.nombre || "Desconocido"}`);
-        return;
+        return undefined;
       }
 
       const subtotal = edificio.costo_hora * time;
-
-      return {
-        id_edificio: idEdificio,
-        id_evento: null,
-        id_montaje_elegido: idMontaje,
-        subtotal_alquiler: subtotal,
-      };
+      if (!(selectedEdificios.size === 0 || !idMontaje)){
+        return {
+          id_edificio: idEdificio,
+          id_evento: null,
+          id_montaje_elegido: idMontaje,
+          subtotal_alquiler: subtotal,
+        };
+      }
     });
 
     if (sessionStorage.getItem("edificiosDummy") != null) {
