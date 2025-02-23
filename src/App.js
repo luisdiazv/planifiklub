5c60b36e18f4ec7be11984cf4c9e38403bd553da
Jonathan Ochoa <jsochoac@unal.edu.co>
2025-02-23 17:26:12 -0500

    Cambios Mercadopago en proceso

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ import Reservation from './routes/Reservation.jsx';
 import ShowEvent from './routes/ShowEvent.jsx';
 import CalendarioEventos from './routes/CalendarioEventos.jsx';
 import VisorDeCotizaciones from './routes/VisorDeCotizaciones.jsx';
+import ResumenPago from './routes/ResumenPago.jsx'; 
 
 // Configuradores
 import ConfiguradorPaginaClub from './routes/Service/ColorService.jsx';
@@ -84,6 +85,7 @@ function App() {
             <Route path="/app/CalendarioEventos" element={<CalendarioEventos />} />
             <Route path="/app/evento/:id" element={<ShowEvent />} />  {/* Ruta para evento específico */}
             <Route path="/app/VisorDeCotizaciones" element={<VisorDeCotizaciones />} />
+            <Route path="/app/ResumenPago/:id" element={<ResumenPago />} />
 
             {/* Configuradores */}
             <Route path="/app/usuarioConfig" element={<ConfiguradorUsuario />} /> {/* Solo Admin */}
