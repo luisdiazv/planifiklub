7709cd560556a7496c77e56fa5ecdc28fe2935aa
sergio <saparadaa@unal.edu.co>
2025-02-25 17:21:05 -0500

    apphome responsive

--- src/routes/AppHome.jsx
+++ src/routes/AppHome.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import './HomeStyles.css';
+import './AppHomeStyles.css';
 import { ClubInfoContext } from '../context/infoClubContext';
 
 const AppHome = () => {
@@ -17,20 +17,23 @@ const AppHome = () => {
       <div className="logo-container">
         {logo && <img src={logo} alt="Logo Club" className="club-logo" />}
       </div>
-      <div className="club-info">
-        <h1>{clubName}</h1>
-        <p>{clubDesc}</p>
+      <div className="app-home-content">
+        <div className="club-info">
+          <h1>{clubName}</h1>
+          <p>{clubDesc}</p>
+        </div>
+        <div className="buttons-container">
+          {!isLoggedIn ? (
+            <>
+              <button className="btn ingresar">Ingresar</button>
+              <button className="btn registrarse">Registrarse</button>
+            </>
+          ) : (
+            <button className="btn reservar">Reservar</button>
+          )}
+        </div>
       </div>
-      <div className="buttons-container">
-        {!isLoggedIn ? (
-          <>
-            <button className="btn ingresar">Ingresar</button>
-            <button className="btn registrarse">Registrarse</button>
-          </>
-        ) : (
-          <button className="btn reservar">Reservar</button>
-        )}
-      </div>
+
     </div>
   );
 };
