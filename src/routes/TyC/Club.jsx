d4827c1eceb61141fc71384828c370656d66f54e
Luis Diaz <ludiazv@unal.edu.co>
2025-02-24 21:40:35 -0500

    Cambiar texto TerminosCondicionesClub

--- src/routes/TyC/Club.jsx
+++ src/routes/TyC/Club.jsx
@@ -1,9 +1,9 @@
 function TerminosCondicionesClub() {
     return (
         <div>
-            <h1>TÉRMINOS Y CONDICIONES DE USO PARA EL USUARIO FINAL DE PLANIFIKLUB</h1>
+            <h1>TÉRMINOS Y CONDICIONES DE USO PARA EL USUARIO FINAL DE CLUB AFILIADO</h1>
             <p>
-                La presente versión de los Términos y Condiciones establece el marco legal y operativo para el uso de la plataforma digital PlanifiKlub, diseñada para facilitar la búsqueda, reserva y gestión de instalaciones y servicios ofrecidos por clubes afiliados en Colombia. El acceso y utilización de la Plataforma implica la aceptación incondicional y completa de todas las cláusulas aquí dispuestas. Se recomienda al Usuario leer detenidamente cada sección para garantizar el correcto uso del servicio.
+                Espacio para que el club afiliado pueda colocar sus términos y condiciones de uso.
             </p>
         </div>
     )
