79986b37c637a4db9e9034b3ce531e6676f4e717
Jonathan Ochoa <jsochoac@unal.edu.co>
2025-02-25 15:36:21 -0500

    Correcciones importantes productos y servicios extra

--- src/Ctrl/PedidoCtrl.js
+++ src/Ctrl/PedidoCtrl.js
@@ -153,3 +153,23 @@ export const createProductoPedido = async (dummyProducto) => {
       throw new Error("Ocurrió un error al crear la entrada en producto_pedido: " + error.message);
   }
 };
+
+export const getPedidosAdicionalesByIdPedido = async (idPedido) => {
+  try {
+    const { data, error } = await supabase
+      .from("pedido")
+      .select("pedidos_adicionales")
+      .eq("idpedido", idPedido)
+      .single();
+
+    if (error) {
+      console.error("Error obteniendo pedidos adicionales:", error.message);
+      throw new Error("No se pudo obtener los pedidos adicionales: " + error.message);
+    }
+
+    return data.pedidos_adicionales;
+  } catch (error) {
+    console.error("Error interno:", error.message);
+    throw new Error("Ocurrió un error al obtener los pedidos adicionales: " + error.message);
+  }
+};
