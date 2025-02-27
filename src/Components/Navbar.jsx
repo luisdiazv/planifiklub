7bc11006d4084019365206a2cceefcd8c7736278
Luis Diaz <ludiazv@unal.edu.co>
2025-02-26 20:51:48 -0500

    Añadido Nav Bar dinamico

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -100,16 +100,21 @@ class Navbar extends Component {
 
     return (
       <nav className="NavbarItems">
-        <Link className="nav-link-logo" to="/app" onClick={this.closeDropdown}>
+        <Link
+          className="nav-link-logo"
+          to={isAppPage ? "/app/" : "/"}
+          onClick={this.closeDropdown}
+        >
           <div className="logoContainer">
             {logo && <img src={logo} alt="Logo" />}
-            <h1 className="navbar-logo"> {clubName}</h1>
+            <h1 className="navbar-logo">{clubName}</h1>
           </div>
         </Link>
         <div className="burguer-menu-container" ref={this.menuRef}>
           <button
-            className={`${isAppPage && currentUser ? "burguer-button-menu-isIn" : "burguer-button-menu"
-              } ${isDropdownVisible ? "active" : ""}`}
+            className={`${isAppPage && currentUser ? "burguer-button-menu-isIn" : "burguer-button-menu"} ${
+              isDropdownVisible ? "active" : ""
+            }`}
             onClick={this.toggleDropdown}
           >
             <i className="fa-solid fa-bars"></i>
